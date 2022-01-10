8409ecf28feafce8722ba6feb884f287d7aa470a
Tanutcha-Am <93970154+Tanutcha-Am@users.noreply.github.com>
2022-01-10 23:28:06 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+#Defind a variable OUTPUT to file name backup_ followed by the date indicated by %Y%m%d and the file type is .tgz in the home directory.
 OUTPUT=~/backup_$(date +%Y%m%d).tgz
+#Unarchive a file OUTPUT to the directory argument 1.
 tar -cvzf $OUTPUT $1
